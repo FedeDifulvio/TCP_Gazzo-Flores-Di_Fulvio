cc62710b6e23bee8355c2b0ef41d72999f2b85e0
FedeDifulvio <71615110+FedeDifulvio@users.noreply.github.com>
2021-07-12 20:35:46 -0300

    Calendario y resaltado de dias que el medico trabaja. Algunos estilos

--- Presentacion/Turnos/NuevoTurno.aspx
+++ Presentacion/Turnos/NuevoTurno.aspx
@@ -5,58 +5,53 @@
      <div class="container-fluid" style="background-color:#f3e9d2; height:90vh">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-4" style="justify-content: center; display: flex">
+                <div class="col-md-4 mt-4 mb-4" style="justify-content: center; display: flex">
                     <h1>Nuevo Turno</h1>
                 </div>
             </div>
 
-           
-            <div class="row"  style="text-align:center">
-                <div class="col-md-3">
-                    
+            <div class="row">
+                <div class="col-md-4">
                 </div>
-                    
-                <div class="col-md-3">
+
+                <div class="col-md-3" style="text-align: center;">
                     <asp:TextBox ID="DniTxt" PlaceHolder="Dni del paciente" MaxLength="8" ClientIDMode="Static" runat="server" CssClass="form-control" />
 
                     <div>
-                            <h1 class="error-form" id="DniTxt-inv"></h1>
-                   </div>
+                        <h1 class="error-form" id="DniTxt-inv"></h1>
+                    </div>
                 </div>
 
 
                 <div class="col-md-3">
-                   <asp:Button ID="DniBtn" Text="Buscar" runat="server" OnClientClick="return ValidarDniPaciente()" onclick="DniBtn_Click" CssClass="btn btn-primary" />
+                    <asp:Button ID="DniBtn" Text="Buscar" runat="server" OnClientClick="return ValidarDniPaciente()" OnClick="DniBtn_Click" CssClass="btn btn-primary" />
 
                 </div>
 
-                <div class="col-md-3">
-
+                <div class="col-md-2">
                 </div>
-
-
 
             </div>
-             <div class="row mt-3"  style="text-align:center">
+             <div class="row mt-2"  style="text-align:center">
                  <asp:Label ID="DniLBL" CssClass="alert alert-danger" Text="Dni erroneo o no encontrado" visible="false" runat="server" />
 
             </div>
 
             <div class="row mb-5" >
                 <div class="col-md-6">
-                    <asp:Label ID="NombreLbl" Text="Nombre" runat="server" visible="false" />
-                    <asp:TextBox ID="NombreTxt" Enabled="false" visible="false" runat="server" CssClass="form-control" />
+                    <asp:Label ID="NombreLbl" Text="Nombre" CssClass="label-turno" runat="server" visible="false" />
+                    <asp:TextBox ID="NombreTxt" Enabled="false" visible="false" runat="server" CssClass="form-control mb-3" />
 
-                    <asp:Label ID="ApellidoLbl" Text="Apellido" runat="server" visible="false" />
-                    <asp:TextBox ID="ApellidoTxt" Enabled="false" visible="false" runat="server" CssClass="form-control" />
+                    <asp:Label ID="ApellidoLbl" Text="Apellido"  CssClass="label-turno" runat="server" visible="false" />
+                    <asp:TextBox ID="ApellidoTxt" Enabled="false" visible="false" runat="server" CssClass="form-control mb-3" />
                 </div>
 
                 <div class="col-md-6">
 
-                    <asp:Label ID="DniLabel" Text="Dni" runat="server" visible="false" />
-                    <asp:TextBox ID="dniTextBox" Enabled="false" visible="false" runat="server" CssClass="form-control"/>
+                    <asp:Label ID="DniLabel" Text="Dni"  CssClass="label-turno" runat="server" visible="false" />
+                    <asp:TextBox ID="dniTextBox" Enabled="false" visible="false" runat="server" CssClass="form-control mb-3"/>
 
-                    <asp:Label ID="ObraSocialLbl" Text="Obra social" runat="server" visible="false" />
+                    <asp:Label ID="ObraSocialLbl" Text="Obra social"  CssClass="label-turno" runat="server" visible="false" />
                     <asp:TextBox ID="ObraSocialTxt" Enabled="false" visible="false" runat="server" CssClass="form-control" />
                 </div>
 
@@ -66,19 +61,29 @@
             <div class="row">
 
                 <div class="col-md-6">
-                    <asp:Label ID="Especialidades" Text="Seleccionar especialidad" visible="false" runat="server"   />
+                    <asp:Label ID="Especialidades" Text="Seleccionar Especialidad"  CssClass="label-turno2" visible="false" runat="server"   />
 
-                    <asp:DropDownList ID="DdlEspecialidades" visible="false"   runat="server" AutoPostBack="true" OnSelectedIndexChanged="DdlEspecialidades_SelectedIndexChanged">
-                       
+                    <asp:DropDownList ID="DdlEspecialidades" visible="false"   runat="server" AutoPostBack="true" AppendDataBoundItems="true" OnSelectedIndexChanged="DdlEspecialidades_SelectedIndexChanged">
                     </asp:DropDownList>
                 </div>
 
                 <div class="col-md-6">
-                    <asp:label ID="LblMedico" text="Seleccionar medico" visible="false" runat="server" />
-                    <asp:DropDownList ID="DdlMedicos" visible="false" runat="server">
+                    <asp:label ID="LblMedico" text="Seleccionar Médico" visible="false"  CssClass="label-turno2" runat="server" />
+                    <asp:DropDownList ID="DdlMedicos" visible="false" AutoPostBack="true" AppendDataBoundItems="true" OnTextChanged ="DdlMedicos_TextChanged" runat="server">
                         
                     </asp:DropDownList>
 
+                </div>
+            </div> 
+
+            <div class="row">
+                <div class="col-md-6 mt-5">
+                    <asp:Calendar ID="Calendario" Visible ="false" OnDayRender="Calendario_DayRender" OnSelectionChanged="Calendario_SelectionChanged"  runat="server"></asp:Calendar>
+                </div>
+                <div class="col-md-6 mt-5"> 
+                    <asp:Label ID="lblFecha"  visible ="false" Text="" runat="server" />
+                    <br />
+                    <asp:DropDownList ID="ddlHorarios" Visible=" false" runat="server"></asp:DropDownList>
                 </div>
             </div>
 
--- Presentacion/Turnos/NuevoTurno.aspx.cs
+++ Presentacion/Turnos/NuevoTurno.aspx.cs
@@ -18,14 +18,23 @@ namespace Presentacion
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                DdlEspecialidades.DataSource = EspecialidadesDDL.Listar();
+                DdlEspecialidades.DataValueField = "ID";
+                DdlEspecialidades.DataTextField = "Nombre";
+                DdlEspecialidades.DataBind();
+            }
+          
         }
 
         protected void DniBtn_Click(object sender, EventArgs e)
         {
+
             string Dni = DniTxt.Text;
 
             Paciente = PacienteNegocio.Listar().Find(x => x.DNI == Dni);
+            Session.Add("PacienteTurno", Paciente); 
 
             if (Paciente== null)
             {
@@ -54,26 +63,155 @@ namespace Presentacion
             LblMedico.Visible = true;
             DdlMedicos.Visible = true;
 
-
+            DdlEspecialidades.Items.Clear();
+            DdlEspecialidades.Items.Insert(0 , "--Seleccione Dato--" );
             DdlEspecialidades.DataSource = EspecialidadesDDL.Listar();
             DdlEspecialidades.DataValueField = "ID";
             DdlEspecialidades.DataTextField = "Nombre";
             DdlEspecialidades.DataBind();
+            
+        }
 
             
-        
+          
+        protected void DdlEspecialidades_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int idEspecialidad = int.Parse(DdlEspecialidades.SelectedItem.Value);
+                DdlMedicos.Items.Clear();
+                DdlMedicos.Items.Insert(0, "--Seleccione Médico--");
+                DdlMedicos.DataSource = MedicoNegocio.Listar().FindAll(x => validadEspecialidad(x.ID, idEspecialidad));
+                DdlMedicos.DataValueField = "ID";
+                DdlMedicos.DataTextField = "Apellido";
+                DdlMedicos.DataBind();
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+            
+
+        }  
+
+
+
+        public bool validadEspecialidad(int idMedico, int idEspecialidad )
+        {
+           
+            List<ObraSocialDeMedico> ObrasDeMedico = MedicoNegocio.ListarObrasSocialesMedico(idMedico);
+            Paciente paciente = (Paciente)Session["PacienteTurno"]; 
+           if( ObrasDeMedico.Find(x=>x.obraSocial.ID == paciente.ObraSocial.ID)== null)
+           {
+                return false; 
+           }
+
+            List<EspecialidadDeMedico> EspecialidadesMedico = MedicoNegocio.ListarEspecialidadesMedico(idMedico);
+
+            if (EspecialidadesMedico.Find(x => x.especialidad.ID == idEspecialidad) == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true; 
+            }
 
         }
 
-        protected void DdlEspecialidades_SelectedIndexChanged(object sender, EventArgs e)
+        protected void DdlMedicos_TextChanged(object sender, EventArgs e)
         {
+            Calendario.Visible = true;
+            int idMedico =int.Parse(DdlMedicos.SelectedValue);
+            List<DiaHorarioTrabajo> DiasMedico = MedicoNegocio.ListarDiasHorariosMedicos(idMedico); 
 
-            int id = int.Parse(DdlEspecialidades.SelectedItem.Value);
-            DdlMedicos.DataSource = MedicoNegocio.Listar().FindAll(x => x.ID == id);
-            DdlMedicos.DataValueField = "ID";
-            DdlMedicos.DataTextField = "Apellido";
-            DdlMedicos.DataBind();
+           /* foreach(var item in DiasMedico)
+            {
+                switch (item.idDia)
+                {
+                    case 1: 
+                }
+            }*/
+
+        }
+
+        protected void Calendario_DayRender(object sender, DayRenderEventArgs e)
+        {
+            int idMedico = int.Parse(DdlMedicos.SelectedValue);
+            List<DiaHorarioTrabajo> DiasMedico = MedicoNegocio.ListarDiasHorariosMedicos(idMedico);
+
+            if(e.Day.Date < DateTime.Today)
+            {
+                e.Day.IsSelectable = false; 
+            }
+
+
+            foreach (var item in DiasMedico)
+             {
+                 switch (item.idDia)
+                 {
+                    case 1:
+                        if ( e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Monday )
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+                        
+                        break;
+                    case 2:
+                        if (e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Tuesday )
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+                       
+                        break;
+                    case 3:
+                        if (e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Wednesday)
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+                      
+                        break;
+                    case 4:
+                        if (e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Thursday )
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+
+                        break;
+
+                    case 5:
+                        if (e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Friday)
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+
+                        break;
+
+                    case 6:
+                        if (e.Day.Date > DateTime.Today && e.Day.Date.DayOfWeek == DayOfWeek.Saturday )
+                        {
+                            e.Cell.BackColor = System.Drawing.Color.Yellow;
+                        }
+
+                        break;
+
+
+                }
+             } 
+
+            if(e.Cell.BackColor != System.Drawing.Color.Yellow)
+            {
+                e.Day.IsSelectable = false;
+            }
+    
+        }
+
+        protected void Calendario_SelectionChanged(object sender, EventArgs e)
+        {
+            lblFecha.Visible = true;
+            lblFecha.Text = "Horarios disponibles para el día " + Calendario.SelectedDate.ToShortDateString(); 
+            ddlHorarios.Visible = true; 
 
         }
     }
